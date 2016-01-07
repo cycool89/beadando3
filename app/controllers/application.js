1143ad7dda8142df8d70f1579b2a10c66cdb8fd4
cycool89 <cycool89@gmail.com>
2016-01-07 11:36:25 +0000

    doksi nélkül, de kész

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,66 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Controller.extend({
-    title: "TODO List Application"
+    title: "TODO List Application",
+    actions: {
+        toggle(id,title,description,aid) {
+            this.set('toggleDisabled',true);
+            document.getElementById("id_edit").value = id;
+            document.getElementById("title_edit").value = title;
+            document.getElementById("description_edit").value = description;
+            document.getElementById("assignedto_edit").value = aid;
+        },
+        editTodo(){
+            var id = document.getElementById("id_edit").value;
+            var select = document.getElementById("assignedto_edit");
+            var formData = {
+                title: document.getElementById("title_edit").value,
+                description: document.getElementById("description_edit").value,
+                assignedto: {
+                    id: select.value,
+                    name: select.options[select.selectedIndex].text
+                }
+            };
+            var _this = this;
+            this.store.find('todo',id).then(function(todo){
+                _this.store.find('person',select.value).then(function(person){
+                    todo.set('assignedto',person);
+                    todo.set('title',formData.title);
+                    todo.set('description',formData.description);
+                    todo.save().then(function(){
+                        _this.set('toggleDisabled',false);
+                    });
+                });
+            });
+            
+        },
+        deleteTodo(itemid){
+            console.log(itemid);
+            this.store.find('todo', itemid).then(function (item) {
+              item.destroyRecord();
+            });
+        },
+        submit(){
+            this.set('toggleDisabled',false);
+            var select = document.getElementById("assignedto");
+            var formData = {
+                title: document.getElementById("title").value,
+                description: document.getElementById("description").value
+            };
+            var todo = this.store.createRecord(
+                'todo', 
+               formData
+            );
+            var person = this.store.find('person',select.value);
+            todo.set('assignedto',person);
+            todo.save().then(function(){
+                person.get('todos').then(function(todos){
+                    todos.pushObject(todo);
+                    todos.save();
+                    person.save();
+                });
+            });
+        }
+    },
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        return {
-            todos(){ return this.store.findAll('todo');},
-            people(){ return this.store.findAll('person');}
+    model(){
+        //return this.store.findAll('todo');
+        var todos = this.store.findAll('todo');
+        var people = this.store.findAll('person');
+        return Ember.Object.create({
+            todos: todos,
+            people: people
+        });
+    },
+    actions: {
+        editTodo(item){
+            console.log(item);
+        },
+        deleteTodo(item){
+            item.deleteRecord();
+            item.save();
+        },
+        addTodo(){
+            this.get('onSave')({
+                //ez azért nem this.$(...).val() mert nem ismeri fel a this.$ -t
+                title: document.getElementById("title").value,
+                description: document.getElementById("description").value,
+                assignedto: document.getElementById("description").value,
+            });
         }
-    }
+    },
 });
