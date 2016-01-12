457ba675e2f6b9403172c63eaa784037ff813ac2
cycool89 <cycool89@gmail.com>
2016-01-12 12:46:04 +0000

    javtás

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,22 +9,5 @@ export default Ember.Route.extend({
             todos: todos,
             people: people
         });
-    },
-    actions: {
-        editTodo(item){
-            console.log(item);
-        },
-        deleteTodo(item){
-            item.deleteRecord();
-            item.save();
-        },
-        addTodo(){
-            this.get('onSave')({
-                //ez azért nem this.$(...).val() mert nem ismeri fel a this.$ -t
-                title: document.getElementById("title").value,
-                description: document.getElementById("description").value,
-                assignedto: document.getElementById("description").value,
-            });
-        }
-    },
+    }
 });
